d61aa36b3810ad874d36b8b171b1a7f4cc108cd4
xianghairui <xianghairui@jd.com>
2018-06-14 17:05:12 +0800

    .

--- java-core/src/main/java/com/xiang/mapper/TblUserMapper.java
+++ java-core/src/main/java/com/xiang/mapper/TblUserMapper.java
@@ -1,30 +1,30 @@
-package com.xiang.mapper;
-
-import com.xiang.model.TblUser;
-import com.xiang.model.TblUserExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
-
-public interface TblUserMapper {
-    long countByExample(TblUserExample example);
-
-    int deleteByExample(TblUserExample example);
-
-    int deleteByPrimaryKey(Long id);
-
-    int insert(TblUser record);
-
-    int insertSelective(TblUser record);
-
-    List<TblUser> selectByExample(TblUserExample example);
-
-    TblUser selectByPrimaryKey(Long id);
-
-    int updateByExampleSelective(@Param("record") TblUser record, @Param("example") TblUserExample example);
-
-    int updateByExample(@Param("record") TblUser record, @Param("example") TblUserExample example);
-
-    int updateByPrimaryKeySelective(TblUser record);
-
-    int updateByPrimaryKey(TblUser record);
-}
+//package com.xiang.mapper;
+//
+//import com.xiang.model.TblUser;
+//import com.xiang.model.TblUserExample;
+//import java.util.List;
+//import org.apache.ibatis.annotations.Param;
+//
+//public interface TblUserMapper {
+//    long countByExample(TblUserExample example);
+//
+//    int deleteByExample(TblUserExample example);
+//
+//    int deleteByPrimaryKey(Long id);
+//
+//    int insert(TblUser record);
+//
+//    int insertSelective(TblUser record);
+//
+//    List<TblUser> selectByExample(TblUserExample example);
+//
+//    TblUser selectByPrimaryKey(Long id);
+//
+//    int updateByExampleSelective(@Param("record") TblUser record, @Param("example") TblUserExample example);
+//
+//    int updateByExample(@Param("record") TblUser record, @Param("example") TblUserExample example);
+//
+//    int updateByPrimaryKeySelective(TblUser record);
+//
+//    int updateByPrimaryKey(TblUser record);
+//}
